f192162aa0c47f0504f8921efad96ccce3a3d05a
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-06-17 11:31:29 +0530

    induce error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Hi B49WEENG');
 console.log('Officially we have started learning Full Stack Web Development');
 
-console.log("added a line change from Remote");
+console.log("added a lie change from Remote");
 
 console.log('Added Line change from local');
